cfa0ffef21edf247ea8f41d24942f50cdb32bfbf
= <aishmeengangyan@gmail.com>
2025-04-09 22:44:43 +0530

    addition done

--- aiqu.py
+++ aiqu.py
@@ -1,8 +1,12 @@
+from addition import add
 print("Hello!")
 a=int(input("Enter 1st no.: "))
 b=int(input("Enter 2nd no.: "))
-print("""Here are some operations that you can perform in this:
+c=input("""Here are some operations that you can perform in this:
 1. Press 1 for addition 
 2. Press 2 for subtraction 
 3. Press 3 for multiplication
-4. Press 4 for division""")
+4. Press 4 for division
+""")
+if c=="1":
+    print(add(a,b))
